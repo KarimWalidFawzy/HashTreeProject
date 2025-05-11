9ed16bef650232db5df6101759c78ad39a10c47b
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2025-05-11 23:54:48 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,8 +2,64 @@
 /**
  * 
  */
+HWND globalMainWindow;
+HMENU hMenu;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+//void AddMenus(HWND);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    
+    WNDCLASSA wc = {0};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "MainWindowClass";
+    RegisterClassA(&wc);
+    globalMainWindow = CreateWindowExA(
+        0,
+        "MainWindowClass",
+        "Hash Tree Example",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+    );
     // Create a hash tree
+    if (globalMainWindow == NULL)
+    {
+        return 0;
+    }
+    ShowWindow(globalMainWindow, nCmdShow);
+
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_CREATE:
+            //AddMenus(hwnd);
+            break;
+        case WM_COMMAND:
+            switch (LOWORD(wParam)) {
+                case 1: // File -> Open
+                    // Handle file open
+                    break;
+                case 2: // File -> Exit
+                    PostQuitMessage(0);
+                    break;
+            }
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProcA(hwnd, uMsg, wParam, lParam);
+    }
     return 0;
 }
